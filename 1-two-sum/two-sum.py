c3d712f2578daf80796a4e142a11b9d9d7b8b10f
abudi-09 <156515083+abudi-09@users.noreply.github.com>
2025-03-08 21:46:09 +0300

    Time: 0 ms (100.00%) | Memory: 19.2 MB (11.43%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        lookup = {}
+        for i, num in enumerate(nums):
+            if target - num in lookup:
+                return [lookup[target - num], i]
+            lookup[num] = i
+        return []
+
+    def twoSum2(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.twoSum([2, 7, 11, 15], 9))
+    print(solution.twoSum2([2, 7, 11, 15], 9))
