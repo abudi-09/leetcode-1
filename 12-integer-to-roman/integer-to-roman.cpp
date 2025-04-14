16b37f86e368ab9e5dacffe2d464f558518b46cb
abudi-09 <156515083+abudi-09@users.noreply.github.com>
2025-04-14 23:58:05 +0300

    Update integer-to-roman.cpp

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,11 +1,11 @@
- class Solution {
+  class Solution {
 public:
     string intToRoman(int num) {
          vector<string> cs = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
           vector<int> vs = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        string ans;
+         string ans;
         for (int i = 0; i < cs.size(); ++i) {
-            while (num >= vs[i]) {
+             while (num >= vs[i]) {
                 num -= vs[i];
                 ans += cs[i];
             }
